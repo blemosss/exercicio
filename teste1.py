2dbab4a9f5549a49e00507000ff120ee7b01d753
blemosss <brunolemos.blo@gmail.com>
2025-01-30 22:14:36 +0000

    calculo

---
+++ teste1.py
@@ -0,0 +1,15 @@
+a = 3
+b = 5
+
+resultado = (2 * a) * (3 * b)
+print("Resultado:", resultado)
+
+# Def das variáveis
+a = 3
+b = 5
+
+# Calc da expressão
+resultado = (2 * a) * (3 * b)
+
+# resultado
+print("Resultado:", resultado)
