70d8e99286cc8773b996a0cef6735ea13e2b4244
blemosss <brunolemos.blo@gmail.com>
2025-02-04 22:08:47 +0000

    exercicios

--- teste.py
+++ teste.py
@@ -1,1 +1,24 @@
 print("Bruno Oliveira")
+
+
+
+#resolvendo calculos KISS
+print(5+10)
+
+print(3*7, (17-2) *8)
+
+print(2**16)
+
+print(37/3)
+
+print(37//3)
+
+print(37 %3)
+
+#teste de veracidade
+print (nome)
+in(qual é o seu nome?)
+print("Olá, (nome))")
+if nome == "Matheus"
+print("que nome bonito!)")
+
---
+++ teste2.py
@@ -0,0 +1,1 @@
+print=input(introduza sua idade)
